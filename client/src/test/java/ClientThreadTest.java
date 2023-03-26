f905590c8a983fd06b488b6cbc21eb828254d92f
TiagoNoite <83978848+TiagoNoite@users.noreply.github.com>
2023-03-26 16:30:14 +0100

    Update ClientThreadTest.java

--- client/src/test/java/ClientThreadTest.java
+++ client/src/test/java/ClientThreadTest.java
@@ -5,30 +5,75 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.concurrent.Semaphore;
 class ClientThreadTest {
-    @Nested
+       @Nested
     @DisplayName("construct the client and writer")
-    class clients{
-
-        private Write_file write;
+    class clients {
         private ArrayList<String> Buffer = new ArrayList<String>();
-        private Semaphore Write_sem  = new Semaphore(1);
-
-        private ClientThread client= new ClientThread(8888,1,1, Buffer, Write_sem);
+        private Semaphore Write_sem = new Semaphore(1);
+        private Semaphore End_process = new Semaphore(1);
+        private Write_file write = new Write_file(Buffer, End_process);
+        private ClientThread client;
 
         @BeforeEach
         void setUp() {
-
+            this.client= new ClientThread(8888, Buffer, Write_sem, End_process);
         }
+
         @Test
         void run() {
-            assertTimeout(Duration.ofSeconds(2) , () ->  client.run());
+           // assertTimeout(Duration.ofSeconds(2), () -> client.run());
         }
+
         @Test
-        void producer_Thread() {
-            //assertThrows(IOException.class, ()->client.producer_Thread());
-            //assertThrows(InterruptedException.class, ()->client.producer_Thread());
-            //assertTimeout(Duration.ofSeconds(2) , () ->  client.producer_Thread());
+        void getClientState() {
+            assertTimeout(Duration.ofSeconds(2), () -> client.getClientState());
+        }
+
+        @Test
+        void activateState() {
+            assertTimeout(Duration.ofSeconds(1), () -> client.activateState());
+        }
+
+        @Test
+        void endState() {
+            assertTimeout(Duration.ofSeconds(1), () -> client.endState());
+        }
+
+        @Test
+        void getMaxNumberClients() {
+            assertTimeout(Duration.ofSeconds(1), () -> client.getMaxNumberClients());
+        }
+
+        @Test
+        void exit() {
+           // assertTimeout(Duration.ofSeconds(2), () -> client.exit());
+        }
+
+        @Test
+        void changeMaxClients() {
+            assertTimeout(Duration.ofSeconds(2), () -> client.changeMaxClients());
+        }
+
+        @Test
+        void writeMessage() {
+            assertTimeout(Duration.ofSeconds(9), () -> client.writeMessage());
+        }
+
+        @Test
+        void addToFilther() {
+            assertTimeout(Duration.ofSeconds(2), () -> client.addToFilther());
+        }
+
+        @Test
+        void menu() {
+            assertTimeout(Duration.ofSeconds(2), () -> client.menu());
+        }
+
+        @Test
+        void showMenu() {
+            assertTimeout(Duration.ofSeconds(2), () -> client.showMenu());
         }
 
     }
+
 }
