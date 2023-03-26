d3bc8346361fcf92fb42e6366cf0e64d931d6459
TiagoNoite <83978848+TiagoNoite@users.noreply.github.com>
2023-03-26 16:31:06 +0100

    Update Write_fileTest.java

--- client/src/test/java/Write_fileTest.java
+++ client/src/test/java/Write_fileTest.java
@@ -3,14 +3,15 @@ import java.time.Duration;
 import java.util.ArrayList;
 import org.junit.jupiter.api.*;
 class Write_fileTest {
-    @Nested
+@Nested
     @DisplayName("construct the writer")
     class writer {
 
         private ArrayList<String> Buffer = new ArrayList<String>();
         private ArrayList<String> Buffer2 = new ArrayList<String>();
-        private Write_file write = new Write_file(Buffer, "File_to_write.txt") ;
-        private Write_file write2 = new Write_file( Buffer2, "a") ;
+        private Semaphore End_process  = new Semaphore(1);
+        private Write_file write = new Write_file(Buffer, End_process) ;
+        private Write_file write2 = new Write_file( Buffer2,End_process ) ;
 
 
         @BeforeEach
