aa992922d8aff8c6eaecb7930e675f2bf61f82ff
TiagoNoite <83978848+TiagoNoite@users.noreply.github.com>
2023-03-27 23:31:35 +0100

    Update Read_FilesTest.java

--- server/src/test/java/Read_FilesTest.java
+++ server/src/test/java/Read_FilesTest.java
@@ -6,7 +6,7 @@ class Read_FilesTest {
     @Nested
     @DisplayName("construct the file reader")
     class reader {
-        private Read_Files readFiles = new Read_Files("./filter.txt");
+        private Read_Files readFiles = new Read_Files("C:\\Users\\pc\\Desktop\\universidade\\3_ano\\PA\\praticas\\repos_grupo1_proj1_PA-Final\\filter.txt");
 
         @Test
         void read_file() {
