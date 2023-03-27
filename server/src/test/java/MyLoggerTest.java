d5f63f20f10c1640cf8047f75127db9aa38e5027
TiagoNoite <83978848+TiagoNoite@users.noreply.github.com>
2023-03-27 22:18:25 +0100

    Update MyLoggerTest.java

--- server/src/test/java/MyLoggerTest.java
+++ server/src/test/java/MyLoggerTest.java
@@ -1,8 +1,8 @@
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.util.logging.FileHandler;
@@ -12,58 +12,33 @@ import java.util.logging.SimpleFormatter;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyLoggerTest {
+    @Nested
+    @DisplayName("construct the logger")
+    class test {
+        private MyLogger testLogger;
 
-    private MyLogger testLogger;
+        @BeforeEach
+        public void test() {
+            try {
+                Logger testLogger = Logger.getLogger("MyLogger");
+                FileHandler fh = new FileHandler("../server.log", true);
+                SimpleFormatter formatter = new SimpleFormatter();
+                fh.setFormatter(formatter);
+                testLogger.addHandler(fh);
+                this.testLogger = new MyLogger();
+            } catch (Exception e) {
+                System.out.println(e);
+            }
 
-    @BeforeAll
-    public void test(){
-        try {
-            Logger testLogger = Logger.getLogger("MyLogger");
-            FileHandler fh = new FileHandler("../server.log", true);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fh.setFormatter(formatter);
-            testLogger.addHandler(fh);
-            this.testLogger = new MyLogger();
         }
-        catch (Exception e){
-            System.out.println(e);
+
+        @Test
+        void logNewMessage() {
+            assertTimeout(Duration.ofSeconds(3), () -> testLogger.logNewMessage(LogType.CONNECTION, 1, "ola"));
+            assertTimeout(Duration.ofSeconds(3), () -> testLogger.logNewMessage(LogType.DISCONNECTION, 1, "ola"));
+            assertTimeout(Duration.ofSeconds(3), () -> testLogger.logNewMessage(LogType.WAITING, 1, "ola"));
+            assertTimeout(Duration.ofSeconds(3), () -> testLogger.logNewMessage(LogType.MESSAGE, 1, "ola"));
         }
-
     }
 
-    @Test
-    @Tag("Server.log test")
-    @DisplayName("Log Connection")
-    public void serverLogTestCon(){
-        this.testLogger.logNewMessage(LogType.CONNECTION, 0, null);
-        assertTimeout(Duration.ofSeconds(3), () -> {
-            this.testLogger.logNewMessage(LogType.CONNECTION, 0, null);
-        });
-    }
-    @Test
-    @Tag("Server.log test")
-    @DisplayName("Log Disconnection")
-    public void serverLogTestDis(){
-        assertTimeout(Duration.ofSeconds(3), () -> {
-            this.testLogger.logNewMessage(LogType.DISCONNECTION, 0, null);
-        });
-    }
-
-    @Test
-    @Tag("Server.log test")
-    @DisplayName("Log Waiting")
-    public void serverLogTestWai(){
-        assertTimeout(Duration.ofSeconds(3), () -> {
-            this.testLogger.logNewMessage(LogType.WAITING, 0, null);
-        });
-    }
-
-    @Test
-    @Tag("Server.log test")
-    @DisplayName("Log Message")
-    public void serverLogTestMes(){
-        assertTimeout(Duration.ofSeconds(3), () -> {
-            this.testLogger.logNewMessage(LogType.MESSAGE, 0, "this is a message");
-        });
-    }
 }
